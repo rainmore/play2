21e643536892fac46916adefd9d156d9e184d07f
Jie Rong <rainmore24@gmail.com>
2013-10-19 00:50:06 +1100

    update to play 2.2

---
+++ app/models/kpi/Templates.scala
@@ -0,0 +1,31 @@
+package models.kpi
+
+import play.api.db.slick.Config.driver.simple._
+
+case class Templates(id: Option[Long], name: String, description: String, isActive: Boolean)
+
+class Template extends Table[(Long, String, String, Boolean)]("kpiTemplate"){
+  def id = column[Long]("id")
+  def name = column[String]("name")
+  def description = column[String]("description")
+  def isActive = column[Boolean]("isActive")
+
+  var sections: List[Section] = _
+
+  override def toString = {
+    "Template [id: %d, name = %s, isActive = %s]".format(id, name, isActive.toString)
+  }
+}
+
+object Templates {
+  def apply(id: Option[Long], name: String, description: String, isActive: Boolean) = {
+    val t: Templates = new Templates
+    t.id = id.getOrElse(0)
+    t.name = name
+    t.description = description
+    t.isActive = isActive
+    t
+  }
+
+  def unapply(t: Templates) = Some((Option(t.id), t.name, t.description, t.isActive))
+}
